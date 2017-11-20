982f364942775a7ab24ef73a882dad66542e1c01
EL TAHHAN Sana <sana.eltahhan@hotmail.fr>
2017-11-20 11:17:51 +0100

    Persist Aeroport

--- src/main/java/Seeder.java
+++ src/main/java/Seeder.java
@@ -1,6 +1,9 @@
+import metier.Aeroport;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
 
 public class Seeder {
     public static void main(String [] args){
@@ -9,6 +12,31 @@ public class Seeder {
         em.getTransaction().begin();
 
 
+        Aeroport a1 = new Aeroport();
+        a1.setNomAeroport("Aéroport international de Conakry");
+        a1.setTaxeAeroport((int)(Math.random() * 1000 + 150));
+
+        Aeroport a2 = new Aeroport();
+        a2.setNomAeroport("Aéroport international Léopold-Sédar-Senghor");
+        a2.setTaxeAeroport((int)(Math.random() * 1000 + 150));
+
+        Aeroport a3 = new Aeroport();
+        a3.setNomAeroport("Aéroport international de Banjul - Yundum");
+        a3.setTaxeAeroport((int)(Math.random() * 1000 + 150));
+
+        Aeroport a4 = new Aeroport();
+        a4.setNomAeroport("Aéroport international Félix-Houphouët-Boigny ");
+        a4.setTaxeAeroport((int)(Math.random() * 1000 + 150));
+
+        Aeroport a5 = new Aeroport();
+        a5.setNomAeroport("Aéroport international de Douala");
+        a5.setTaxeAeroport((int)(Math.random() * 1000 + 150));
+
+        Aeroport a6 = new Aeroport();
+        a6.setNomAeroport("Aéroport international de Gaborone");
+        a6.setTaxeAeroport((int)(Math.random() * 1000 + 150));
+
+        
 
         em.getTransaction().commit();
         em.close();
