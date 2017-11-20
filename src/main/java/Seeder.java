07f1be74e6f9a37f259a9595a133ce8f26067640
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-11-20 11:13:04 +0100

    add vols

--- src/main/java/Seeder.java
+++ src/main/java/Seeder.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import metier.Vol;
 import util.TypeVol;
-=======
 import metier.Abonnement;
->>>>>>> f8fdd42b4e882a570dca86370d075d81e341c58d
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,7 +16,6 @@ public class Seeder {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
-<<<<<<< HEAD
         Set<Vol> vols = new HashSet<Vol>();
         Date dateDepart = new Date (2017, 01, 01);
         Date dateDepartVolsRegulier = new Date (2017, 01, 01);
@@ -43,17 +39,9 @@ public class Seeder {
             em.persist(vol);
         }
 
-
-
-
-
-
-
-=======
         Abonnement ab = new Abonnement("Abonnement 1", 15095.82f);
         Abonnement ab2 = new Abonnement("Abonnement 2", 18795.82f);
         Abonnement ab3 = new Abonnement("Abonnement 3", 19595.82f);
->>>>>>> f8fdd42b4e882a570dca86370d075d81e341c58d
 
         em.persist(ab);
         em.persist(ab2);
