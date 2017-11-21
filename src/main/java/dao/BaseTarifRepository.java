2de6671201c4522f2b5974b341ebacee0c988544
Khaloudinho <khaled.bouguettoucha5@etu.univ-lorraine.fr>
2017-11-21 14:35:38 +0100

    fix error in repositories

--- src/main/java/dao/BaseTarifRepository.java
+++ src/main/java/dao/BaseTarifRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface BaseTarifRepository extends JpaRepository<Lieu, Long> {
+public interface BaseTarifRepository extends JpaRepository<BaseTarif, Long> {
 
     @Query(value = "SELECT * FROM basetarif WHERE typeBaseTarif = :nom", nativeQuery = true)
     BaseTarif getBaseTarifParNom(@Param("nom") String nom);
