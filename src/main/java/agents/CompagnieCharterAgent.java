d7e0e289da77d03900044cbd574c6a1090a0fe8f
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-11-23 22:13:46 +0100

    fix:CopagnieContainer:agent error

--- src/main/java/agents/CompagnieCharterAgent.java
+++ src/main/java/agents/CompagnieCharterAgent.java
@@ -1,23 +1,16 @@
 package agents;
 
-import behaviors.vols.RegisterVolBehavior;
+import behaviors.vols.RegisterAgentBehavior;
 import behaviors.vols.VolManagementBehavior;
 import containers.CompagnieContainer;
 import jade.core.AID;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.core.behaviours.OneShotBehaviour;
 import jade.core.behaviours.ParallelBehaviour;
-import jade.domain.DFService;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
-import jade.domain.FIPAException;
 import jade.gui.GuiAgent;
 import jade.gui.GuiEvent;
 import jade.lang.acl.ACLMessage;
 import jade.wrapper.ControllerException;
 
 public class CompagnieCharterAgent extends GuiAgent implements Compagnie {
-
     private CompagnieContainer compagnieContainer;
 
 
@@ -28,7 +21,7 @@ public class CompagnieCharterAgent extends GuiAgent implements Compagnie {
         System.out.println("Initialisation de l'agent "+this.getAID().getName());
 
         ParallelBehaviour parallelBehaviour = new ParallelBehaviour();
-        parallelBehaviour.addSubBehaviour(new RegisterVolBehavior());
+        parallelBehaviour.addSubBehaviour(new RegisterAgentBehavior("Vols", "Vols-Association"));
         parallelBehaviour.addSubBehaviour(new VolManagementBehavior(compagnieContainer));
 
         addBehaviour(parallelBehaviour);
--- src/main/java/behaviors/vols/RegisterAgentBehavior.java
+++ src/main/java/behaviors/vols/RegisterAgentBehavior.java
@@ -7,23 +7,29 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 
-public class RegisterVolBehavior extends OneShotBehaviour{
+public class RegisterAgentBehavior extends OneShotBehaviour{
 
-    public RegisterVolBehavior() {
+    private String serviceType;
+    private String serviceName;
+
+    public RegisterAgentBehavior(String serviceType, String serviceName) {
         super();
+        this.serviceName = serviceName;
+        this.serviceType = serviceType;
     }
 
+    //Attention fait planter avec plusieurs agents
     @Override
     public void action() {
         System.out.println("*************************************************");
         DFAgentDescription dfa=new DFAgentDescription();
         dfa.setName(super.myAgent.getAID());
         ServiceDescription sd = new ServiceDescription();
-        sd.setType("Vols");
-        sd.setName("Vols-Associations");
+        sd.setType(serviceType);
+        sd.setName(serviceName);
         dfa.addServices(sd);
         try {
-            DFService.register(myAgent, dfa);
+            DFService.register(super.myAgent, dfa);
         } catch (FIPAException e) {
             e.printStackTrace();
         }
