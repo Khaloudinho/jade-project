f2a9d09bb2824a903aecbacddb2ae22cdb33f805
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-11-29 18:07:55 +0100

    start ticker behavior and give work to khaled

---
+++ src/main/java/behaviors/UpdateFlightsPrices.java
@@ -0,0 +1,27 @@
+package behaviors;
+
+import dao.Seeder;
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import util.TypeVol;
+
+public class UpdateFlightsPrices extends TickerBehaviour {
+
+    public UpdateFlightsPrices(Agent a, long period) {
+        super(a, period);
+    }
+
+    @Override
+    protected void onTick() {
+        //On va regarder la liste des vols charters
+        Seeder.getVols(TypeVol.Charter, )
+
+        //On va regarder la date courante
+        //On regarde la date de depart
+        //on fait la formule magique pour l'update
+
+    }
+}
+
+
+
